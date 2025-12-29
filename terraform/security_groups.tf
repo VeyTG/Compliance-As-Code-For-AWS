272340c1a93dad424fbb643021f61dd75380fa11
VeyTG <vutuongvy0202@gmail.com>
2025-12-30 03:25:05 +0700

    fix: secure security group

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "compliant" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # 10.0.0.0/8
+    cidr_blocks = ["10.0.0.0/16"] # 10.0.0.0/8
   }
 
   # ✅ COMPLIANT: HTTPS từ internal
